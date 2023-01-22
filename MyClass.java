fa8f98c8b08d819199d4c75a6225f994b46348e9
ElenaPopova12 <leonoria@ukr.net>
2023-01-22 19:58:45 +0200

    HW1

---
+++ MyClass.java
@@ -0,0 +1,13 @@
+class MyClass {
+    int year = 2023;
+
+    public static int sumOfTwoNumbers(int a, int b) {
+        int c = a + b; //sum of 2 numbers
+        return c;
+    }
+
+    public static void main(String[] args) {
+        sumOfTwoNumbers(5, 7);
+
+    }
+}
