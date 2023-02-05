1152cec73ef7feb5dfdf50f941254d21e92317ae
ElenaPopova12 <leonoria@ukr.net>
2023-02-05 09:38:45 +0200

    four files homework 4

---
+++ other/ClassOtherPack.java
@@ -0,0 +1,10 @@
+package other;
+
+import employee.Employee;
+
+public class ClassOtherPack {
+    public static void main(String[] args) {
+        Employee empl = new Employee("Popova");
+        System.out.println(empl.surname);
+    }
+}
